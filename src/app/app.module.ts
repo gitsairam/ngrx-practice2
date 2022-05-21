50597c8a75a4397d81cc2c62eae370906210e0a9
gitsairam <sairamvenkata194@gmail.com>
2022-05-21 23:04:49 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { bookReducer } from './state/book.reducer';
 import { collectionReducer } from './state/collection.reducer';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
+import { BookEffects } from './state/book.effects';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
 
@@ -14,6 +16,7 @@ import { BookCollectionComponent } from './book-collection/book-collection.compo
   imports: [
     BrowserModule,
     StoreModule.forRoot({ books: bookReducer, collection: collectionReducer }),
+    EffectsModule.forRoot([BookEffects]),
     HttpClientModule,
   ],
   declarations: [AppComponent, BookListComponent, BookCollectionComponent],
