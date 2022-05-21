cd8b4f61865661348180f939bfcade9c73049260
gitsairam <sairamvenkata194@gmail.com>
2022-05-21 14:19:04 +0530

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { bookReducer } from './state/book.reducer';
 import { collectionReducer } from './state/collection.reducer';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  imports: [BrowserModule,StoreModule.forRoot({books:bookReducer,collection:collectionReducer})],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ books: bookReducer, collection: collectionReducer }),
+    HttpClientModule,
+  ],
+  declarations: [AppComponent, BookListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/state/book.selectors.ts
+++ src/app/state/book.selectors.ts
@@ -4,3 +4,11 @@ import { Book } from '../book-list/book.model';
 export const selectBooks = createFeatureSelector<ReadonlyArray<Book>>('books');
 export const selectCollectionState =
   createFeatureSelector<ReadonlyArray<string>>('collection');
+
+export const selectBookCollection=createSelector(selectBooks,selectCollectionState,(books,collection)=>{
+  collection.map((id)=>{
+    books.find(book)=>{
+      book.id===id
+    }
+  })
+})
